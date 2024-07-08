b5a963825582361ec42829c4333b98a4e1b17787
Fariddun Kadirov <90576595+kadirov-Fariddun@users.noreply.github.com>
2024-07-08 15:10:03 +0500

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 export default function Home() {
   const [teams,setTeams] = useState([]);
   const getTeams = async () => {
-    const URL = 'https://protocol-mpliga-server-7hnl.vercel.app/api/teams/';
+    const URL = 'http://45.84.225.47:5001/api/teams/';
     let data = [];
     try{
       const response = await axios.get(URL);
