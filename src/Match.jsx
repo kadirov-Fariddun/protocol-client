bf4eb9d0c5bfa5e07b3a44e8d3d400ca96fffd1f
Fariddun Kadirov <kadirovbrucelee@gmail.com>
2024-07-11 19:13:03 +0500

    update see protocol

--- src/Match.jsx
+++ src/Match.jsx
@@ -15,6 +15,7 @@ export const Match = () => {
     const [playerAlertModalDisplay,setPlayerAlertModal] = useState('');
     const [playerAlert,setPlayerAlert] = useState('');
     const [endMatchModal,setEndMatchModal] = useState('');
+    const [seeProtocol,setSeeProtocol] = useState(false);
     // обновляем данные игрока и команды в туре 
     const updateDataTeam = async (id,newData) => {
       try{
@@ -537,12 +538,17 @@ const sendMessageToTelegram = (playerAlert,team1,team2,player) => {
       </div>
     </div>
     { Boolean(team1.ended) && Boolean(team2.ended) ?
-      <div className="admin-modal">
+      <div className={`admin-modal ${seeProtocol?'see':''}`}>
         <BackBtn />
         <p>Ushbu uchrashuv o'z nixoyasiga yetgan</p>
         <p>agar biror bir narsa xato ketgan bo'lsa </p>
         <p><strong>Fariddun brat</strong> ga murojat qiling </p>
         <p>Xurmat bilan <strong>Fariddun</strong></p>
+        <button className={`protocol-see-btn ${seeProtocol?'see':''}`} onClick={()=>seeProtocol?setSeeProtocol(false):setSeeProtocol(true)}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+              <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/>
+            </svg>
+        </button>
       </div>
       :''
     }
