55c1ffb61f1e4368a7db6107e1a7e793dc6ac12d
Fariddun Kadirov <90576595+kadirov-Fariddun@users.noreply.github.com>
2024-07-18 16:55:33 +0500

    Add files via upload

---
+++ src/GoTable.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export const GoTable = () => {
+  return (
+    <NavLink to='/table' className='table-link'>Tablitsa</NavLink>
+  )
+}
